6f3e0edf57c3eaaea4632597a0b888ed8f721910
Anthony Nguyen <Nguyen.Anthonyc@gmail.com>
2018-02-09 11:54:57 -0700

    modified create workout

--- src/components/CreateWorkout.js
+++ src/components/CreateWorkout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FlatList, StyleSheet, Text, View, ScrollView } from 'react-native';
+import { FlatList, StyleSheet, Text, View, } from 'react-native';
 import { Button, CardSection } from './common';
 
 
